94ca2e4131d76c5bd3b56f1e62ec87a9e67bccb8
sina-ashtari <sinaa1383@gmail.com>
2024-08-30 19:42:12 +0330

    Added New Material Colors

--- app/src/main/java/xyz/sina/dowr/ui/theme/Color.kt
+++ app/src/main/java/xyz/sina/dowr/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package xyz.sina.dowr.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrussianBlue = Color(0, 48, 73)
+val FireEngineRed = Color(214, 40, 40)
+val Orange = Color(247, 127, 0)
+val Xanthous = Color(252, 191, 73)
+val Vanilla = Color(234, 226, 183)
