64b237f6e9571097f96cc2d15bea86b98689a110
sina-ashtari <sinaa1383@gmail.com>
2024-08-30 19:41:51 +0330

    Added SharedPreferences For OnBoarding Screen

---
+++ app/src/main/java/xyz/sina/dowr/tutorial/TutorialUtils.kt
@@ -0,0 +1,12 @@
+package xyz.sina.dowr.tutorial
+
+import android.content.Context
+
+class TutorialUtils (private val context : Context) {
+    fun isBoardingCompleted():Boolean {
+        return context.getSharedPreferences("onBoardingCompleted", Context.MODE_PRIVATE).getBoolean("completed", false)
+    }
+    fun setOnboardingCompleted(){
+        context.getSharedPreferences("onBoardingCompleted", Context.MODE_PRIVATE).edit().putBoolean("completed", true).apply()
+    }
+}
