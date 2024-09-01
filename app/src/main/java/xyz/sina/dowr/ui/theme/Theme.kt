b35d3079cf62730f1cb457bba94e66863558c4f6
sina-ashtari <sinaa1383@gmail.com>
2024-09-01 13:51:12 +0330

    Refactor : Deleted Unused Imports

--- app/src/main/java/xyz/sina/dowr/ui/theme/Theme.kt
+++ app/src/main/java/xyz/sina/dowr/ui/theme/Theme.kt
@@ -1,15 +1,9 @@
 package xyz.sina.dowr.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+
 
 private val colorScheme = darkColorScheme(
     primary = FireEngineRed,
@@ -19,12 +13,11 @@ private val colorScheme = darkColorScheme(
     surfaceBright = Xanthous
 )
 
-
 @Composable
 fun DowrTheme(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
+        MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
