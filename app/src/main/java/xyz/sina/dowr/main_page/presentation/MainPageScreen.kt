b29bcb9320f3f8c1111d2dbaeb6522ef34972fcf
sina-ashtari <sinaa1383@gmail.com>
2024-09-06 14:12:27 +0330

    Added Info AlertDialog

--- app/src/main/java/xyz/sina/dowr/main_page/presentation/MainPageScreen.kt
+++ app/src/main/java/xyz/sina/dowr/main_page/presentation/MainPageScreen.kt
@@ -2,31 +2,43 @@ package xyz.sina.dowr.main_page.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import xyz.sina.dowr.R
-import xyz.sina.dowr.ui.theme.DowrTheme
+import xyz.sina.dowr.core.navigation.InGameSettings
+import xyz.sina.dowr.core.navigation.Tutorial
 
 @Composable
-fun MainPage() {
+fun MainPage(navController: NavHostController) {
+
+    val showAlertDialog = remember { mutableStateOf(false) }
+    if(showAlertDialog.value) { InfoAlertDialog(showAlertDialog = showAlertDialog)}
+
     Scaffold(
         topBar = {
             Row(
@@ -35,15 +47,20 @@ fun MainPage() {
                     .background(color = MaterialTheme.colorScheme.secondary)
                     .padding(start = 8.dp, end = 8.dp)
             ) {
-                IconButton(modifier = Modifier.weight(1f), onClick = {}) {
+                IconButton(modifier = Modifier.weight(1f), onClick = {
+                    showAlertDialog.value = true
+                }) {
                     Icon(
                         imageVector = Icons.Default.Info,
                         contentDescription = null,
                         tint = MaterialTheme.colorScheme.onBackground
                     )
                 }
+
                 Spacer(modifier = Modifier.weight(1f))
-                IconButton(modifier = Modifier.weight(1f), onClick = {}) {
+                IconButton(modifier = Modifier.weight(1f), onClick = {
+                    navController.navigate(Tutorial)
+                }) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_help),
                         contentDescription = null,
@@ -53,16 +70,42 @@ fun MainPage() {
             }
         }
     ) { innerPadding ->
-        Box(modifier = Modifier.fillMaxSize().padding(innerPadding), contentAlignment = Alignment.Center) {
-            Image(painter = painterResource(id = R.drawable.ic_play), contentDescription = null)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(innerPadding), contentAlignment = Alignment.Center
+        ) {
+            Row() {
+                Image(
+                    modifier = Modifier.clickable { navController.navigate(InGameSettings) },
+                    painter = painterResource(id = R.drawable.ic_play),
+                    contentDescription = null,
+                )
+                IconButton(modifier = Modifier, onClick = {
+                    navController.navigate(Tutorial)
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_help),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
         }
     }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MainPreview() {
-    DowrTheme {
-        MainPage()
+fun InfoAlertDialog(
+    showAlertDialog: MutableState<Boolean>
+) {
+    if (showAlertDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showAlertDialog.value = false },
+            title = { Text(modifier = Modifier.fillMaxWidth(), text = "درباره بازی", textAlign = TextAlign.Center)},
+            text = { Text(modifier = Modifier.fillMaxWidth(),text = "نوشته شده با بدبختی", style = TextStyle(textDirection = TextDirection.Rtl))},
+            confirmButton = {})
     }
 }
