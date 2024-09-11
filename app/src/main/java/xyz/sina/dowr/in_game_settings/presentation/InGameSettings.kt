6866659aa55040e9cbc8683558da39d2eac8bc9a
sina-ashtari <sinaa1383@gmail.com>
2024-09-11 18:52:53 +0330

    Added Word Categories Data Class And List Of It

--- app/src/main/java/xyz/sina/dowr/in_game_settings/presentation/InGameSettings.kt
+++ app/src/main/java/xyz/sina/dowr/in_game_settings/presentation/InGameSettings.kt
@@ -1,13 +1,19 @@
 package xyz.sina.dowr.in_game_settings.presentation
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -28,6 +34,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import xyz.sina.dowr.core.navigation.InGame
 import xyz.sina.dowr.core.persentation.ButtonUI
+import xyz.sina.dowr.in_game_settings.data.Categories
+import xyz.sina.dowr.in_game_settings.data.DifficultySelectOption
+import xyz.sina.dowr.in_game_settings.data.WordCategories
 
 @Composable
 fun InGameSettingsScreen(
@@ -106,36 +115,57 @@ fun InGameSettingsScreen(
                     value = inGameSettingViewModel.time.toString(),
                     onValueChange = inGameSettingViewModel::changeTime,
                     label = { Text(text = "زمان برای هر تیم") })
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = { inGameSettingViewModel.changeDifficulty(1) }) {
-                        Text(text = "آسون")
-                    }
-                    Spacer(modifier = Modifier.padding(4.dp))
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = { inGameSettingViewModel.changeDifficulty(2) }) {
-                        Text(text = "متوسط")
-                    }
-                    Spacer(modifier = Modifier.padding(4.dp))
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = { inGameSettingViewModel.changeDifficulty(3) }) {
-                        Text(text = "سخت")
-                    }
-                    Spacer(modifier = Modifier.padding(4.dp))
-                    Button(
-                        modifier = Modifier.weight(1f),
-                        onClick = { inGameSettingViewModel.changeDifficulty(3) }) {
-                        Text(text = "تصادفی")
-                    }
-                }
+                DifficultySingleSelectionList(
+                    options = inGameSettingViewModel.difficultyOptions,
+                    onOptionClicked = inGameSettingViewModel::selectionOptionSelected
+                )
             }
             // categories of words and players word
-            Column(modifier = Modifier.fillMaxWidth()) {
+        }
+        PickWordCategories()
+    }
+}
 
-            }
+@Composable
+fun DifficultySingleSelectionList(
+    options: List<DifficultySelectOption>,
+    onOptionClicked: (DifficultySelectOption) -> Unit
+) {
+    LazyRow(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        items(options) { option ->
+            SingleSelection(option, onOptionClicked)
         }
     }
 }
+
+@Composable
+fun SingleSelection(
+    selectionOption: DifficultySelectOption,
+    onOptionClicked: (DifficultySelectOption) -> Unit
+) {
+    ButtonUI(
+        text = selectionOption.option,
+        onClick = { onOptionClicked(selectionOption) },
+        backgroundColor = if (!selectionOption.selected) Color.White else Color.Gray
+    )
+}
+
+@Composable
+fun PickWordCategories(
+
+) {
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxWidth(),
+        state = rememberLazyGridState(),
+        columns = GridCells.Fixed(3),
+        verticalArrangement = Arrangement.spacedBy(3.dp),
+        horizontalArrangement = Arrangement.spacedBy(3.dp)
+    ) {
+        itemsIndexed(Categories){ index: Int, category ->
+            CategoriesView(wordCategories = category, position = index, onItemClick = { position ->
+
+            })
+        }
+    }
+}
+// should change the code wtf is that dude
