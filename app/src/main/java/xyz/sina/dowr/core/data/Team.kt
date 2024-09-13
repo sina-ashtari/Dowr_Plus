6976fcd94fb578054d074a8c1976d76a6b9adbf4
sina-ashtari <sinaa1383@gmail.com>
2024-09-13 17:26:34 +0330

    Changed Team Data Class

--- app/src/main/java/xyz/sina/dowr/core/data/Team.kt
+++ app/src/main/java/xyz/sina/dowr/core/data/Team.kt
@@ -1,7 +1,9 @@
 package xyz.sina.dowr.core.data
 
-data class Team (
-    val id : Int,
-    val playerPair : Pair<String, String>,
-    val teamTime : Int
+data class Team(
+    val id: Int,
+    val playerPair: Pair<String, String>,
+    val initialTime: Long,
+    var remainingTime: Long,
+    var isTimerRunning: Boolean = false
 )
